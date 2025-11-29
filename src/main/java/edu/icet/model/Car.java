694bc1e7bf62ec80557197c34eb06b0671cb663b
PasiSandeepa <pasisandeepa456@gmail.com>
2025-11-29 21:49:16 +0530

    ADD Car Dto

---
+++ src/main/java/edu/icet/model/Car.java
@@ -0,0 +1,23 @@
+package edu.icet.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Car {
+    private long id;
+    private  String brand;
+    private  String model;
+    private  String color;
+    private  int year;
+    private  double pricePerDay;
+    private String type;
+    private int seats;
+    private String fuelType;
+    private String registrationNo;
+    private Double engineCapacity;
+    private Boolean available = true;
+}

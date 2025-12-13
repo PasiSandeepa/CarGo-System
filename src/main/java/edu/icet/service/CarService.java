1c75778608bc47938cc534e2df08045c49909bf4
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-13 20:54:36 +0530

    Add saveCar Method & Implementation

---
+++ src/main/java/edu/icet/service/CarService.java
@@ -0,0 +1,16 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.car.CarRequestDto;
+import edu.icet.model.dto.car.CarResponseDto;
+
+import java.util.List;
+
+
+public interface CarService {
+    CarResponseDto saveCar(CarRequestDto dto);
+//    CarResponseDto getCar(Long id);
+//    List<CarResponseDto>getAllCars();
+//    List<CarResponseDto>getAvailableCars();
+//    CarResponseDto updateCar(Long id, CarRequestDto dto);
+//    boolean deleteCar(Long id);
+}
---
+++ src/main/java/edu/icet/service/impl/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package edu.icet.service.impl;
+
+import edu.icet.model.dto.car.CarRequestDto;
+import edu.icet.model.dto.car.CarResponseDto;
+import edu.icet.model.entity.Car;
+import edu.icet.repository.CarRepository;
+import edu.icet.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CarServiceImpl implements CarService {
+
+    private final ModelMapper modelMapper;
+    private final CarRepository carRepository;
+
+    @Override
+    public CarResponseDto saveCar(CarRequestDto dto) {
+        Car car = modelMapper.map(dto, Car.class);
+        carRepository.save(car);
+        return modelMapper.map(car, CarResponseDto.class);
+    }
+
+
+}

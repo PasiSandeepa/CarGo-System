261ddde75de7a3926061eb93ae59704a07eba6f9
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-15 15:25:04 +0530

    Map snake_case JSON fields to Car DTO using JsonProperty

--- src/main/java/edu/icet/model/dto/car/CarRequestDto.java
+++ src/main/java/edu/icet/model/dto/car/CarRequestDto.java
@@ -1,21 +1,38 @@
 package edu.icet.model.dto.car;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Setter
 @Getter
-@AllArgsConstructor
+@Setter
 @NoArgsConstructor
-@ToString
+@AllArgsConstructor
 public class CarRequestDto {
     private String brand;
     private String model;
     private String color;
-    private int year;
-    private double pricePerDay;
+    private Integer year;
+    private Double pricePerDay;
     private String type;
-    private int seats;
+    private Integer seats;
     private String fuelType;
     private String registrationNo;
     private Double engineCapacity;
+
+    @JsonProperty("pickup_lat")
+    private Double pickupLat;
+
+    @JsonProperty("pickup_lng")
+    private Double pickupLng;
+
+    @JsonProperty("pickup_address")
+    private String pickupAddress;
+
+    private Boolean available;
+
+    @JsonProperty("image_url")
+    private String imageUrl;
 }

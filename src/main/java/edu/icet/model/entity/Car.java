c424f69a47e96d8ef4f5cfd0f222f4e9da490f48
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-15 15:24:09 +0530

    Add Car entity with JPA mappings

--- src/main/java/edu/icet/model/entity/Car.java
+++ src/main/java/edu/icet/model/entity/Car.java
@@ -11,20 +11,41 @@ import lombok.*;
 @Entity
 @Table(name="cars")
 public class Car {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long carid;
-    private  String brand;
-    private  String model;
-    private  String color;
-    private Integer year;
-    private Double pricePerDay;
+
+    private String brand;
+    private String model;
+    private String color;
+    private Integer year = 0;
+
+    @Column(name = "priceperday")
+    private Double pricePerDay = 0.0;
+
     private String type;
     private Integer seats;
     private String fuelType;
+
+    @Column(name = "registration_no")
     private String registrationNo;
-    private Double engineCapacity;
-    private Double pickupLat;
-    private Double pickupLng;
-    private String pickupAddress;
+
+    @Column(name = "engine_capacity")
+    private Double engineCapacity = 0.0;
+
+    @Column(name = "pickup_lat")
+    private Double pickupLat = 0.0;
+
+    @Column(name = "pickup_lng")
+    private Double pickupLng = 0.0;
+
+    @Column(name = "pickup_address")
+    private String pickupAddress = "";
+
+    @Column(columnDefinition = "bit(1) default 1")
     private Boolean available = true;
+
+    @Column(name = "image_url")
+    private String imageUrl = "";
 }

3bea3c6a94f2f21bc8477a9d138764074e90bc5f
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-19 14:28:34 +0530

    Implement  login  Method

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -2,6 +2,7 @@ package edu.icet.controller;
 
 
 import edu.icet.model.dto.customer.CustomerRequestDto;
+import edu.icet.model.dto.customer.CustomerResponseDto;
 import edu.icet.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,5 +20,13 @@ public class CustomerController {
         customerService.addCustomer(dto);
         return ResponseEntity.ok("Customer Registered Successfully");
     }
-
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody CustomerRequestDto dto) {
+        try {
+            CustomerResponseDto response = customerService.login(dto.getEmail(), dto.getPassword());
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        }
+    }
 }

8e5a2b7982cef71ea6569889a57cafafdc9cc63f
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-23 15:25:44 +0530

    Implement Method getNotificationsByCustomer

--- src/main/java/edu/icet/service/NotificationService.java
+++ src/main/java/edu/icet/service/NotificationService.java
@@ -1,7 +1,11 @@
 package edu.icet.service;
 
 import edu.icet.model.dto.notification.NotificationRequestDto;
+import edu.icet.model.dto.notification.NotificationResponseDto;
+
+import java.util.List;
 
 public interface NotificationService {
    void createNotification(NotificationRequestDto requestDto);
+   List<NotificationResponseDto> getNotificationsByCustomer(Long customerId);
 }
--- src/main/java/edu/icet/service/impl/NotificationServiceImpl.java
+++ src/main/java/edu/icet/service/impl/NotificationServiceImpl.java
@@ -1,11 +1,15 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.dto.notification.NotificationRequestDto;
+import edu.icet.model.dto.notification.NotificationResponseDto;
 import edu.icet.repository.NotificationRepository;
 import edu.icet.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -18,5 +22,19 @@ public class NotificationServiceImpl implements NotificationService {
     public void createNotification(NotificationRequestDto requestDto) {
         edu.icet.entity.notification.Notification notification = modelMapper.map(requestDto, edu.icet.entity.notification.Notification.class);
         repository.save(notification);
+    }
+
+    @Override
+    public List<NotificationResponseDto> getNotificationsByCustomer(Long customerId) {
+        List<edu.icet.entity.notification.Notification> entities = repository.findByCustomerIdOrderBySendDateDesc(customerId);
+
+        List<NotificationResponseDto> responseList = new ArrayList<>();
+
+
+        for (edu.icet.entity.notification.Notification entity : entities) {
+            responseList.add(modelMapper.map(entity, NotificationResponseDto.class));
+        }
+
+        return responseList;
     }
 }

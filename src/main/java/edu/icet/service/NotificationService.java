43dbf1ff429681d0a87dc5790ff1354556630a52
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-23 15:21:52 +0530

    Implement Method createNotification

--- src/main/java/edu/icet/service/NotificationService.java
+++ src/main/java/edu/icet/service/NotificationService.java
@@ -1,4 +1,7 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.notification.NotificationRequestDto;
+
 public interface NotificationService {
+   void createNotification(NotificationRequestDto requestDto);
 }
--- src/main/java/edu/icet/service/impl/NotificationServiceImpl.java
+++ src/main/java/edu/icet/service/impl/NotificationServiceImpl.java
@@ -1,6 +1,22 @@
 package edu.icet.service.impl;
 
+import edu.icet.model.dto.notification.NotificationRequestDto;
+import edu.icet.repository.NotificationRepository;
 import edu.icet.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository repository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public void createNotification(NotificationRequestDto requestDto) {
+        edu.icet.entity.notification.Notification notification = modelMapper.map(requestDto, edu.icet.entity.notification.Notification.class);
+        repository.save(notification);
+    }
 }
